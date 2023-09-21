fb080d7af8937e37793b5630e891059e0cc5bb80
Hendra Anggrian <hendraanggrian@gmail.com>
2023-09-21 14:54:34 -0500

    Google recommends JS column width 80, forced semicolon

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,17 +1,17 @@
-const LOCAL_STORAGE_KEY = 'minimal-dark-theme'
+const LOCAL_STORAGE_KEY = 'minimal-dark-mode';
 
 if (isDarkMode()) {
-  document.getElementsByTagName('html')[0].classList.add('theme-dark')
+  document.getElementsByTagName('html')[0].classList.add('theme-dark');
 }
 
 function isDarkMode() {
-  const storage = localStorage.getItem(LOCAL_STORAGE_KEY)
-  return storage ? JSON.parse(storage) : false
+  const storage = localStorage.getItem(LOCAL_STORAGE_KEY);
+  return storage ? JSON.parse(storage) : false;
 }
 
 function toggleDarkMode() {
-  document.getElementsByTagName('html')[0].classList.toggle('theme-dark')
-  const flippedDarkMode = !isDarkMode()
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(flippedDarkMode))
-  return flippedDarkMode
+  document.getElementsByTagName('html')[0].classList.toggle('theme-dark');
+  const flippedDarkMode = !isDarkMode();
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(flippedDarkMode));
+  return flippedDarkMode;
 }
